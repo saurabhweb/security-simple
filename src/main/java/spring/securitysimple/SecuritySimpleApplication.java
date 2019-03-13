e5464630701a5352e254115d3ec0e3ead2b4f522
Saurabh Agarwal <sagarwal@castlighthealth.com>
2019-03-12 21:31:53 -0700

    created skeleton from spring.io - with web,security

---
+++ src/main/java/spring/securitysimple/SecuritySimpleApplication.java
@@ -0,0 +1,13 @@
+package spring.securitysimple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuritySimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuritySimpleApplication.class, args);
+	}
+
+}
